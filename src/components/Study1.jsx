5b7b1f6f174c91711923bc2a8363c260e447ba99
Srushti Salvi <srushtisalvi4@gmail.com>
2023-05-31 13:22:46 +0530

    Study section

---
+++ src/components/Study1.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Study1 = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-3 justify-between my-6">
+        <div data-aos="fade-right" className=" p-6 font-medium text-2xl">
+          Be the best you with EQ
+        </div>
+        <div className=" p-6 text-gray-600 text-lg">
+          {" "}
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s{" "}
+        </div>
+        <div className=" p-6 text-gray-600 text-lg">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Study1;
